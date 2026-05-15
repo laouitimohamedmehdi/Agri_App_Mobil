5e4a4edeb6568295b91ba4bbf0bc7fca71c3fd5e
laouitimohamedmehdi <medmehdi.laouiti@gmail.com>
2026-05-15 11:10:34 +0200

    feat(mobile/offline): intégrer NetworkProvider et OfflineBanner dans App.js

--- App.js
+++ App.js
@@ -4,22 +4,27 @@ import { PaperProvider } from 'react-native-paper';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { AuthProvider } from './src/contexts/AuthContext';
 import { SettingsProvider } from './src/contexts/SettingsContext';
+import { NetworkProvider } from './src/contexts/NetworkContext';
 import RootNavigator from './src/navigation/RootNavigator';
+import OfflineBanner from './src/components/OfflineBanner';
 import { agriproTheme } from './src/theme';
 import { navigationRef } from './src/navigation/navigationRef';
 
 export default function App() {
   return (
     <SettingsProvider>
-      <GestureHandlerRootView style={{ flex: 1 }}>
-        <PaperProvider theme={agriproTheme}>
-          <NavigationContainer ref={navigationRef}>
-            <AuthProvider>
-              <RootNavigator />
-            </AuthProvider>
-          </NavigationContainer>
-        </PaperProvider>
-      </GestureHandlerRootView>
+      <NetworkProvider>
+        <GestureHandlerRootView style={{ flex: 1 }}>
+          <PaperProvider theme={agriproTheme}>
+            <NavigationContainer ref={navigationRef}>
+              <AuthProvider>
+                <OfflineBanner />
+                <RootNavigator />
+              </AuthProvider>
+            </NavigationContainer>
+          </PaperProvider>
+        </GestureHandlerRootView>
+      </NetworkProvider>
     </SettingsProvider>
   );
 }
